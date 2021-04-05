291c0fe0a5688c9d75339fda9224fafb9717f5c6
zerohope <a584171811@gmail.com>
2021-04-05 08:56:14 +0800

    debug  the question

--- src/GrokkingCoding/KPairsWithLargestSums.java
+++ src/GrokkingCoding/KPairsWithLargestSums.java
@@ -1,2 +1,35 @@
-package GrokkingCoding;public class KPairsWithLargestSums {
+package GrokkingCoding;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class KPairsWithLargestSums {
+    public static List<int[]> findKLargestPairs(int[] nums1,int[] nums2,int k){
+        PriorityQueue<int[]> minHeap=new PriorityQueue<>((a,b)->a[0]+a[1]-b[0]-b[1]);
+        for(int i=0;i<Math.min(nums1.length,k);i++)
+            for (int j=0;j<Math.min(nums2.length,k);j++){
+                if(minHeap.size()<k)
+                    minHeap.offer(new int[]{nums1[i],nums2[j]});
+                else{
+                    if(nums1[i]+nums2[j]<minHeap.peek()[0]+minHeap.peek()[1])
+                        break;
+                    else{
+                        minHeap.poll();
+                        minHeap.offer(new int[]{nums1[i],nums2[j]});
+                    }
+                }
+            }
+            return new ArrayList<>(minHeap);
+    }
+
+  public static void main(String[] args){
+
+        int[] l1=new int[]{9,8,2};
+        int[] l2=new int[]{100,3,1};
+        List<int[]> result=findKLargestPairs(l1,l2,3);
+       for(int[] pair:result)
+           System.out.println(pair[0]+" "+pair[1]);
+  }
+
 }
--- src/GrokkingCoding/Knapsack.java
+++ src/GrokkingCoding/Knapsack.java
@@ -1,2 +1,58 @@
-package GrokkingCoding;public class Knapsack {
+package GrokkingCoding;
+
+public class Knapsack {
+    static Integer[][] memory=new Integer[4][8];
+    public static int knapsackRecursive(int[] profits, int[] weights, int capacity, int currentIndex){
+
+        if(currentIndex>=profits.length||capacity<=0)
+           return 0;
+        if(memory[currentIndex][capacity]!=null)
+            return memory[currentIndex][capacity];
+        int profit1=0,profit2;
+        if(capacity>=weights[currentIndex])
+        profit1=profits[currentIndex]+knapsackRecursive(profits,weights,capacity-weights[currentIndex],currentIndex+1);
+
+        profit2=knapsackRecursive(profits,weights,capacity,currentIndex+1);
+        memory[currentIndex][capacity]=Math.max(profit1,profit2);
+       return memory[currentIndex][capacity];
+    }
+    public static int dpmethod(int[] profits, int[] weights, int capacity){
+        int[][] maxprofits=new int[2][capacity+1];
+
+        for(int j=1;j<=capacity;j++)
+            maxprofits[0][j]=1;
+        for(int i=1;i<profits.length;i++)
+            for (int j=1;j<=capacity;j++){
+               int profit1=0,profit2=0;
+                if(weights[i]<=j)
+                    profit1=profits[i]+maxprofits[(i-1)&1][j-weights[i]];
+                 profit2=maxprofits[(i-1)&1][j];
+                 maxprofits[i&1][j]=Math.max(profit1,profit2);
+            }
+        return maxprofits[3&1][7];
+    }
+public static int dpoptimize(int[] profits, int[] weights, int capacity){
+        int[] maxProfits=new int[capacity+1];
+        for(int i=0;i<capacity+1;i++)
+            if(weights[0]<=i)
+            maxProfits[i]=profits[0];
+
+        for(int i=1;i<profits.length;i++)
+            for(int j=capacity;j>-1;j--){
+                int profit1=0,profit2=0;
+                if(weights[i]<=j)
+                    profit1=profits[i]+maxProfits[j-weights[i]];
+                profit2=maxProfits[j];
+                maxProfits[j]=Math.max(profit1,profit2);
+            }
+
+
+    return maxProfits[capacity];
+}
+    public static void main(String[] args){
+        int[] profits={1,6,10,16};
+        int[] weights={1,2,3,5};
+        System.out.println(dpoptimize(profits,weights,7));
+
+    }
 }
