73b1f4b655763de29b45e03463d8342995bdc30c
zerohope <a584171811@gmail.com>
2021-05-07 09:00:15 +0800

    贪心加dp

--- src/interviewQuestion/RiverCrossing.java
+++ src/interviewQuestion/RiverCrossing.java
@@ -3,7 +3,7 @@ package interviewQuestion;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class RiverCrossing {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
