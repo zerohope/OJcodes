95f5b4ed3f7258679d31610ddbae9b40be8e70ce
zerohope <a584171811@gmail.com>
2021-09-30 09:54:19 +0800

    leetcode 1012

--- src/leetcode/NumDupDigitsAtMostN.java
+++ src/leetcode/NumDupDigitsAtMostN.java
@@ -1,12 +1,50 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumDupDigitsAtMostN {
     public int numDupDigitsAtMostN(int n) {
-
+        int[] used=new int[10];
+        List<Integer> digits=new ArrayList<>();
+        while(n>0){
+            digits.add(n%10);
+            n=n/10;
+        }
+        int total=0;
+        int k=digits.size();
+        for(int i=1;i<k;i++){
+            total+=9*A(9,i-1);
+        }
+        for(int i=k-1;i>=0;i--){
+            int num=digits.get(i);
+           for(int j=i==k-1?1:0;j<num;j++){
+               if(used[j]!=0){
+                   continue;
+               }
+            total+=A(10-(k-i),i);
+           }
+           if(++used[num]>1)
+               break;
+            if (i==0){
+                total = total+1;
+            }
+        }
+        return total;
     }
 
 
 
+    public static int fact(int n){
+         if(n==1||n==0)
+            return 1;
+        return n*fact(n-1);
+    }
+
+    public int A(int m, int n){
+
+        return fact(m)/fact(m-n);
+    }
 
 
 }
