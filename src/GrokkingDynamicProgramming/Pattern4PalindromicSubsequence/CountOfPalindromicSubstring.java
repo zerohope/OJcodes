796460339cc047d72e741581aa7d895a32f94d59
zerohope <a584171811@gmail.com>
2022-04-17 09:43:40 +0800

    增加了dfs实现

--- src/GrokkingDynamicProgramming/Pattern4PalindromicSubsequence/CountOfPalindromicSubstring.java
+++ src/GrokkingDynamicProgramming/Pattern4PalindromicSubsequence/CountOfPalindromicSubstring.java
@@ -1,6 +1,9 @@
 package GrokkingDynamicProgramming.Pattern4PalindromicSubsequence;
 
+import java.util.Arrays;
+
 public class CountOfPalindromicSubstring {
+    int[][] dp;
     public int countSubstrings(String s) {
         int rs=0;
         boolean[][] dp=new boolean[s.length()][s.length()];
@@ -19,8 +22,33 @@ public class CountOfPalindromicSubstring {
             }
             return rs;
     }
+    public int answer(String s){
+        dp=new int[s.length()][s.length()];
+       //Arrays.fill(dp,-1);
+        return dfs(s,0,s.length()-1);
+    }
+    public int dfs(String s,int i,int j){
+        if(dp[i][j]!=-1)
+            return 0;
+        if(i>j)
+             return 0;
+        if(i==j)
+            return 1;
+       int cur=check(s,i,j);
+       int res=dp[i][j-1]+dp[i-1][j]+cur;
+       dp[i][j]=res;
+        return dp[i][j];
+    }
+    public int check(String s, int i, int j){
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;j--;
+            }else{
+                return 0;
+            }
+        }
+        return 1;
 
-
-
+    }
 
 }
